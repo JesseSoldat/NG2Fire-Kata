5ee26efa5c78c11e5dd734c8b99eda2e0996e2b9
jesse <jsoldat@hotmail.com>
2017-03-30 16:41:45 +0700

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { AngularFireModule, AuthProviders, AuthMethods } from 'angularfire2';
+import { FlashMessagesModule } from 'angular2-flash-messages';
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 
 import { AppComponent } from './app.component';
 
